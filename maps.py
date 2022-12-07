21fb2b8e1f82d5a35a6d12911ade614d59d0ab21
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-12-07 14:05:04 -0500

    added decompression

--- maps.py
+++ maps.py
@@ -2,6 +2,7 @@ import PySimpleGUI as psg
 import random
 import os
 import subprocess
+import shutil
 
 if (os.path.exists("LEGO BATTLES") == False):
     filename = psg.popup_get_file("Enter your ROM file:", file_types = [("NDS Files", "*.nds")])
@@ -15,3 +16,35 @@ Then just X out of NitroPaint, and it will immediately open again for the next o
     subprocess.run([ "NitroPaint.exe", "LEGO BATTLES/data/KingTileset.NCGR", "LEGO BATTLES/data/KingTileset.NCLR" ])
     subprocess.run([ "NitroPaint.exe", "LEGO BATTLES/data/MarsTileset.NCGR", "LEGO BATTLES/data/MarsTileset.NCLR" ])
     subprocess.run([ "NitroPaint.exe", "LEGO BATTLES/data/PirateTileset.NCGR", "LEGO BATTLES/data/PirateTileset.NCLR" ])
+
+if (os.path.exists("Backup Maps") == False):
+    shutil.copytree("LEGO BATTLES/data/Maps", "Backup Maps")
+
+if (os.path.exists("Uncompressed Maps") == False):
+    os.makedirs("Uncompressed Maps")
+    for root, dirs, files in os.walk("LEGO BATTLES/data/Maps"):
+        for file in files:
+            opening = open(os.path.join(root, file), "rb")
+            reading = opening.read()
+            opening.close()
+            
+            newFile = open("./Uncompressed Maps/" + file, "wb")
+            newFile.close()
+            newFile = open("./Uncompressed Maps/" + file, "ab")
+            
+            segNumber = int.from_bytes(reading[8:12], "little")
+            offset = 16 + 4 * segNumber
+            for i in range(segNumber):
+                size = int.from_bytes(reading[(16 + (i * 4)):(20 + (i * 4))], "little")
+                tempFile = open("temp_" + str(i).zfill(3) + ".bin", "wb")
+                tempFile.write(reading[offset:(offset + size)])
+                tempFile.close()
+                subprocess.run([ "lzx.exe", "-d", "temp_" + str(i).zfill(3) + ".bin" ])
+                tempFile = open("temp_" + str(i).zfill(3) + ".bin", "rb")
+                newFile.write(tempFile.read())
+                tempFile.close()
+                os.remove("temp_" + str(i).zfill(3) + ".bin")
+                offset = offset + size
+            newFile.close()
+                
+            
